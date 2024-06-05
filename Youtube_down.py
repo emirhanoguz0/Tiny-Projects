eca802e98ceee1e747805ff7827667d6c267dce2
Emirhan Oğuz <160913617+emirhanoguz0@users.noreply.github.com>
2024-06-05 23:19:02 +0300

    Update Youtube_down.py

--- Youtube_down.py
+++ Youtube_down.py
@@ -1,17 +1,30 @@
 from pytube import YouTube
+import tkinter as tk
+from tkinter import messagebox
+
+def get_entry_data():
+    url = entry1.get()
+    YouTube(url).streams.get_highest_resolution().download("C:\\Users\Monster\Videos\Youtube Videos")
 
 try:
+    main_window = tk.Tk()
+    main_window.geometry("400x200")
+    main_window.title("YouTube Video Downloader")
 
-    #input the YouTube URL
-    url = input("Enter the YouTube URL: ")
+    label1 = tk.Label(main_window, text="YouTube Url:")
+    label1.pack()
 
-    yt = YouTube(url).streams.get_highest_resolution()
+    entry1 = tk.Entry(main_window, text="")
+    entry1.pack()
 
-    print("Title:", YouTube(url).title)
-    print("Views:", YouTube(url).views)
-    yt.download("C:\\Users\Monster\Videos\Youtube Videos")
+    download_button = tk.Button(main_window, text="Download", command=get_entry_data)
+    download_button.pack()
 
-    print("Download complete.")
 
+    #print("Title:", YouTube(url).title)
+    #print("Views:", YouTube(url).views)
+
+    #print("Download complete.")
+    main_window.mainloop()
 except Exception as e:
     print("An error occurred:", str(e))
