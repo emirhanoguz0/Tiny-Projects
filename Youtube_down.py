5008c8278b199d89851ff874c7779e23fd653c07
Emirhan Oğuz <160913617+emirhanoguz0@users.noreply.github.com>
2024-07-01 19:15:26 +0300

    Update Youtube_down.py

--- Youtube_down.py
+++ Youtube_down.py
@@ -1,17 +1,25 @@
 from pytube import YouTube
 import tkinter as tk
-from tkinter import ttk, messagebox
+from tkinter import ttk, messagebox, filedialog
 import threading
 import time
 
+def get_file_path():
+    file_path = filedialog.askdirectory()
+    threading.Thread(target=update_entry2, args=(file_path,)).start()
+
+def update_entry2(file_path):
+    entry2.delete(0, tk.END)
+    entry2.insert(0,file_path)
 def get_and_download():
     url = entry1.get()
-    threading.Thread(target=down_video, args=(url,)).start()
+    file_path = entry2.get()
+    threading.Thread(target=down_video, args=(url,file_path,)).start()
 
-def down_video(url):
+def down_video(url,file_path):
     yt = YouTube(url, on_progress_callback=data_bar)
     ytvh = yt.streams.get_highest_resolution()
-    ytvh.download("C:\\Users\\Monster\\Videos\\Youtube Videos")
+    ytvh.download(file_path)
     entry1.delete(0, tk.END)
     down_message(url)
 
@@ -41,14 +49,23 @@ try:
     label1 = tk.Label(main_window, text="YouTube Url:", font="Helvetica 11")
     label1.grid(row=1, column=0)
 
-    entry1 = tk.Entry(main_window, width=40)
+    entry1 = tk.Entry(main_window, width=45)
     entry1.grid(row=1, column=1)
 
+    label2 = tk.Label(main_window, text="File Path:", font="Helvetica 11")
+    label2.grid(row=2, column=0)
+
+    entry2 = tk.Entry(main_window, width=45)
+    entry2.grid(row=2, column=1)
+
+    button1 = tk.Button(main_window, text="File Path", font="Helvetica 8" ,command=get_file_path)
+    button1.grid(row=2, column=2)
+
     download_button = tk.Button(main_window, text="Download", command=get_and_download, width=8, height=2)
-    download_button.grid(row=2, column=1)
+    download_button.grid(row=3, column=1)
 
     exit_button = tk.Button(main_window, text="Exit", command=main_window.quit)
-    exit_button.grid(row=3, column=1)
+    exit_button.grid(row=4, column=1)
 
     main_window.mainloop()
 except Exception as e:
